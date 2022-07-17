85a68c449cabae153e838352c7c63f1c17b1559b
Sara Chen <sarachen@outlook.com>
2022-07-17 16:21:37 +1000

    ninia-jobs

---
+++ ninja-jobs/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eum ratione architecto nihil voluptatum pariatur modi excepturi, iste quidem eveniet aspernatur reiciendis totam ut ad voluptatem eaque molestias! Recusandae quos vitae sequi quisquam doloribus culpa eum minus facere soluta voluptatem, suscipit omnis libero mollitia fugiat repudiandae placeat et ex iusto quas non maiores dolorem, cumque quidem. Et id illum numquam, sint est fugit recusandae ipsa ipsam, laudantium voluptatum dolores animi quo debitis unde porro inventore, eveniet laboriosam voluptates adipisci nisi vero at? Temporibus autem impedit facilis odit animi porro ipsam obcaecati, quas perferendis saepe numquam nulla minima! Ipsum, velit beatae.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eum ratione architecto nihil voluptatum pariatur modi excepturi, iste quidem eveniet aspernatur reiciendis totam ut ad voluptatem eaque molestias! Recusandae quos vitae sequi quisquam doloribus culpa eum minus facere soluta voluptatem, suscipit omnis libero mollitia fugiat repudiandae placeat et ex iusto quas non maiores dolorem, cumque quidem. Et id illum numquam, sint est fugit recusandae ipsa ipsam, laudantium voluptatum dolores animi quo debitis unde porro inventore, eveniet laboriosam voluptates adipisci nisi vero at? Temporibus autem impedit facilis odit animi porro ipsam obcaecati, quas perferendis saepe numquam nulla minima! Ipsum, velit beatae.</p>
+  
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
